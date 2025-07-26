8641e9f6f386317422b056bf8fc7cf9909a5930a
w3villa-akhilesh <akhilesh.gupta@w3villa.com>
2025-07-26 15:38:50 +0530

    UI Improved

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import About from './components/About';
 import Services from './components/Services';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+import GlobalClipPaths from './assets/svg/ClipPath';
 
 const App = () => {
 
   return (
     <>
+    <GlobalClipPaths />
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
      <Header />
      <Hero />
